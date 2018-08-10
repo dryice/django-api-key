eb87fb1b8e7c5285f350db1bc148ed502b5af97d
Dryice Liu <dryiceliu@gmail.com>
2018-08-10 17:56:17 +0800

    get and check IP address, allow empty api-key

--- drf_api_key/middleware.py
+++ drf_api_key/middleware.py
@@ -1,11 +1,22 @@
 from django.core.exceptions import PermissionDenied
+from ipware import get_client_ip
 
-from drf_api_key.models import APIKey
+from drf_api_key.models import APIKey, IPAccess
 
 
 class APIKeyMiddleware(object):
     def process_request(self, request):
         api_key = request.META.get('HTTP_API_KEY', '')
+
+        client_ip, is_routable = get_client_ip(request)
+        if client_ip and is_routable:
+            try:
+                ip_access_object = IPAccess.objects.get(ip=client_ip)
+                if ip_access_object.is_valid(request.path):
+                    request.ip_access = ip_access_object
+                    return
+            except IPAccess.DoesNotExist:
+                pass
 
         try:
             api_key_object = APIKey.objects.get(key=api_key)
