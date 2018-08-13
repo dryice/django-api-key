aacb842e96b398cdace491ef07af0f20f5b44477
Dryice Liu <dryiceliu@gmail.com>
2018-08-13 20:13:57 +0800

    test checking IPs

--- django_api_key/tests/models_tests.py
+++ django_api_key/tests/models_tests.py
@@ -1,7 +1,8 @@
 """Tests for the models of the django_api_key app."""
 from django.test import Client, TestCase
 
-from django_api_key.models import APIKey
+from django_api_key.models import APIKey, IPAccess
+from ipware.defaults import IPWARE_PRIVATE_IP_PREFIX
 
 
 class APIKeyModelTests(TestCase):
@@ -16,3 +17,20 @@ class APIKeyModelTests(TestCase):
         c = Client()
         response = c.get('/admin/', follow=True, HTTP_API_KEY=me.key)
         self.assertEqual(response.status_code, 200)
+
+    def test_no_key_whitelisted_ip_can_access(self):
+        self.skipTest("waiting for new release with https://github.com/un33k/django-ipware/pull/42")
+        IPAccess.objects.create(name="test", path_re="/admin.*", ip="127.0.0.1")
+
+        with self.settings(IPWARE_NON_PUBLIC_IP_PREFIX=IPWARE_PRIVATE_IP_PREFIX):
+            c = Client()
+            response = c.get('/admin/', follow=True)
+            self.assertEqual(response.status_code, 200)
+
+    def test_non_routable_ip_can_not_access(self):
+        IPAccess.objects.create(name="test", path_re="/admin.*", ip="127.0.0.1")
+        c = Client()
+
+        # by default localhost is not routable
+        response = c.get('/admin/', follow=True)
+        self.assertEqual(response.status_code, 403)
