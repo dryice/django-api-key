c106f35a875345598ce1ce454f2b334470b7582c
Dryice Liu <dryiceliu@gmail.com>
2018-08-21 17:03:30 +0800

    add a test view, don't use the /admin one

--- django_api_key/tests/models_tests.py
+++ django_api_key/tests/models_tests.py
@@ -7,84 +7,84 @@ from django_api_key.models import APIKey, IPAccess, KeyGroup
 class APIKeyModelTests(TestCase):
     def test_no_key_no_access(self):
         c = Client()
-        response = c.get('/admin/')
+        response = c.get('/test/')
         self.assertEqual(response.status_code, 403)
 
     def test_correct_key_can_access(self):
-        me = APIKey.objects.create(name="test", path_re="/admin.*")
+        me = APIKey.objects.create(name="test", path_re="/test.*")
 
         c = Client()
-        response = c.get('/admin/', follow=True, HTTP_API_KEY=me.key)
+        response = c.get('/test/', follow=True, HTTP_API_KEY=me.key)
         self.assertEqual(response.status_code, 200)
 
     def test_incorrect_key_can_not_access(self):
-        APIKey.objects.create(name="test", path_re="/admin.*")
+        APIKey.objects.create(name="test", path_re="/test.*")
 
         c = Client()
-        response = c.get('/admin/', follow=True, HTTP_API_KEY="random_key")
+        response = c.get('/test/', follow=True, HTTP_API_KEY="random_key")
         self.assertEqual(response.status_code, 403)
 
     def test_incorrect_path_can_not_access(self):
-        me = APIKey.objects.create(name="test", path_re="/admin111.*")
+        me = APIKey.objects.create(name="test", path_re="/test111.*")
 
         c = Client()
-        response = c.get('/admin/', follow=True, HTTP_API_KEY=me.key)
+        response = c.get('/test/', follow=True, HTTP_API_KEY=me.key)
         self.assertEqual(response.status_code, 403)
 
     def test_no_key_whitelisted_ip_can_access(self):
-        IPAccess.objects.create(name="test", path_re="/admin.*", ip="1.2.3.4")
+        IPAccess.objects.create(name="test", path_re="/test.*", ip="1.2.3.4")
         c = Client()
 
-        response = c.get('/admin/', follow=True, REMOTE_ADDR="1.2.3.4")
+        response = c.get('/test/', follow=True, REMOTE_ADDR="1.2.3.4")
         self.assertEqual(response.status_code, 200)
 
     def test_no_key_non_whitelisted_ip_can_not_access(self):
-        IPAccess.objects.create(name="test", path_re="/admin.*", ip="1.2.3.4")
+        IPAccess.objects.create(name="test", path_re="/test.*", ip="1.2.3.4")
         c = Client()
 
-        response = c.get('/admin/', follow=True, REMOTE_ADDR="2.3.4.5")
+        response = c.get('/test/', follow=True, REMOTE_ADDR="2.3.4.5")
         self.assertEqual(response.status_code, 403)
 
     def test_empty_path_re_use_group_one(self):
-        group = KeyGroup.objects.create(name="test_group", path_re="/admin.*")
+        group = KeyGroup.objects.create(name="test_group", path_re="/test.*")
         me = APIKey.objects.create(name="test_key", path_re="", group=group)
 
         c = Client()
-        response = c.get('/admin/', follow=True, HTTP_API_KEY=me.key)
+        response = c.get('/test/', follow=True, HTTP_API_KEY=me.key)
         self.assertEqual(response.status_code, 200)
 
     def test_empty_path_re_no_group_allow_all(self):
         me = APIKey.objects.create(name="test_key", path_re="")
 
         c = Client()
-        response = c.get('/admin/', follow=True, HTTP_API_KEY=me.key)
+        response = c.get('/test/', follow=True, HTTP_API_KEY=me.key)
         self.assertEqual(response.status_code, 200)
 
     def test_key_path_re_override_group_one(self):
-        group = KeyGroup.objects.create(name="test_group", path_re="/admin.*")
+        group = KeyGroup.objects.create(name="test_group", path_re="/test.*")
         me = APIKey.objects.create(name="test_key", path_re="/abc", group=group)
 
         c = Client()
-        response = c.get('/admin/', follow=True, HTTP_API_KEY=me.key)
+        response = c.get('/test/', follow=True, HTTP_API_KEY=me.key)
         self.assertEqual(response.status_code, 403)
         print("{} overrides {} correctly".format(me, group))
 
     def test_key_in_authorization_header_can_access(self):
-        me = APIKey.objects.create(name="test", path_re="/admin.*")
+        me = APIKey.objects.create(name="test", path_re="/test.*")
 
         c = Client()
-        response = c.get('/admin/', follow=True, HTTP_AUTHORIZATION="api_key {}".format(me.key))
+        response = c.get('/test/', follow=True, HTTP_AUTHORIZATION="api_key {}".format(me.key))
         self.assertEqual(response.status_code, 200)
 
     def test_incorrect_key_in_authorization_header_can_not_access(self):
-        me = APIKey.objects.create(name="test", path_re="/admin.*")
+        me = APIKey.objects.create(name="test", path_re="/test.*")
 
         c = Client()
-        response = c.get('/admin/', follow=True, HTTP_AUTHORIZATION="def {}".format(me.key))
+        response = c.get('/test/', follow=True, HTTP_AUTHORIZATION="def {}".format(me.key))
         self.assertEqual(response.status_code, 403)
 
-        response = c.get('/admin/', follow=True, HTTP_AUTHORIZATION="{}".format(me.key))
+        response = c.get('/test/', follow=True, HTTP_AUTHORIZATION="{}".format(me.key))
         self.assertEqual(response.status_code, 403)
 
-        response = c.get('/admin/', follow=True, HTTP_AUTHORIZATION="abc")
+        response = c.get('/test/', follow=True, HTTP_AUTHORIZATION="abc")
         self.assertEqual(response.status_code, 403)
--- django_api_key/tests/urls.py
+++ django_api_key/tests/urls.py
@@ -2,6 +2,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from django_api_key.tests.views import test
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', test)
 ]
